23e61666a49961a1b1b05a43ab33e093286758a0
VIGNESH K <bu181023@gmail.com>
2021-06-14 06:02:34 +0000

    LoginUI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,12 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,6 +27,11 @@ import { LoginComponent } from './login/login.component';
     FlexLayoutModule,
     BrowserAnimationsModule,
     MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
